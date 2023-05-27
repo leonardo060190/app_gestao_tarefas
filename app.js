7299c3f1a5ad905a4df08e89d2e3036a2af9116e
leonardo060190 <leonardo-morruga@hotmail.com>
2023-05-27 19:52:38 -0300

    Adicionado comentarios nos cruds

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //código javascript em app.js
+
+//importando o framework express
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
@@ -25,6 +27,8 @@ connection.connect(function (err) {
 
 // tabela do banco de dados 'TAREFAS'
 // CREATE
+//método post é usado para inclusão, adiciona uma nova tarefa
+//O Comando INSERT INTO inseri linhas na tabela criada.
 app.post('/tarefas', (req, res) => {
     const tarefas = req.body;
     const sql = 'INSERT INTO tarefas SET ?';
@@ -35,6 +39,9 @@ app.post('/tarefas', (req, res) => {
 });
 
 // READ
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela. 
+//Podemos recuperar todos os registros de uma tabela usando um asterisco(*) em uma consulta SELECT.
 app.get('/tarefas', (req, res) => {
     const sql = 'SELECT * FROM tarefas ';
     connection.query(sql, (error, results) => {
@@ -43,6 +50,10 @@ app.get('/tarefas', (req, res) => {
     });
 });
 
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela. 
+//O WHERE é usada em casos de consulta para ajudar a especificar os dados que serão consultados e manipulados.
+//Aqui ele faz a consulta somente no ('id') informado. 
 app.get('/tarefas/:id', (req, res) => {
     const id = req.params.id;
     const sql = 'SELECT * FROM tarefas WHERE id = ?';
@@ -53,17 +64,26 @@ app.get('/tarefas/:id', (req, res) => {
 });
 
 // UPDATE
+//método put é usado para alteração. id indica o registro a ser alterado
+// Update: atualiza os registros presentes nas tabelas SQL. 
+// Assim, utilizaremos o comando UPDATE para fazer alterações nos dados presentes nas tabelas.
+//O WHERE é usada em casos de consulta para ajudar a especificar os dados que serão consultados e manipulados.
+//Aqui ele faz a consulta somente no ('id') informado para fazer a alteraçao.
 app.put('/tarefas/:id', (req, res) => {
     const id = req.params.id;
-    const newtask = req.body;
+    const tarefaAtualizada = req.body;
     const sql = 'UPDATE tarefas SET ? WHERE id = ?';
-    connection.query(sql, [newtask, id], (error) => {
+    connection.query(sql, [tarefaAtualizada, id], (error) => {
         if (error) throw error;
         res.status(204).end();
     });
 });
 
 // DELETE
+// Delete: remove ou deleta os registros das tabelas SQL. 
+// Aqui vai remover apenas os registros específicos que atendem a uma 
+// determinada condição usando O WHERE .
+//O Where aqui ele vai deixar deletar somente os registros indicados pelo ('id').
 app.delete('/tarefas/:id', (req, res) => {
     const id = req.params.id;
     const sql = 'DELETE FROM tarefas WHERE id = ?';
@@ -76,6 +96,9 @@ app.delete('/tarefas/:id', (req, res) => {
 
 // tabela do banco de dados 'PESSOAS'
 // CREATE
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela. 
+//Podemos recuperar todos os registros de uma tabela usando um asterisco(*) em uma consulta SELECT.
 app.post('/pessoas', (req, res) => {
     const pessoas = req.body;
     const sql = 'INSERT INTO pessoas SET ?';
@@ -86,6 +109,9 @@ app.post('/pessoas', (req, res) => {
 });
 
 // READ
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela. 
+//Podemos recuperar todos os registros de uma tabela usando um asterisco(*) em uma consulta SELECT.
 app.get('/pessoas', (req, res) => {
     const sql = 'SELECT * FROM pessoas ';
     connection.query(sql, (error, results) => {
@@ -94,6 +120,10 @@ app.get('/pessoas', (req, res) => {
     });
 });
 
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela. 
+//O WHERE é usada em casos de consulta para ajudar a especificar os dados que serão consultados e manipulados.
+//Aqui ele faz a consulta somente no ('id') informado. 
 app.get('/pessoas/:id', (req, res) => {
     const id = req.params.id;
     const sql = 'SELECT * FROM pessoas WHERE id = ?';
@@ -104,6 +134,11 @@ app.get('/pessoas/:id', (req, res) => {
 });
 
 // UPDATE
+//método put é usado para alteração. id indica o registro a ser alterado
+// Update: atualiza os registros presentes nas tabelas SQL. 
+// Assim, utilizaremos o comando UPDATE para fazer alterações nos dados presentes nas tabelas.
+//O WHERE é usada em casos de consulta para ajudar a especificar os dados que serão consultados e manipulados.
+//Aqui ele faz a consulta somente no ('id') informado para fazer a alteraçao.
 app.put('/pessoas/:id', (req, res) => {
     const id = req.params.id;
     const nova_pessoa = req.body;
@@ -115,6 +150,10 @@ app.put('/pessoas/:id', (req, res) => {
 });
 
 // DELETE
+// Delete: remove ou deleta os registros das tabelas SQL. 
+//Aqui vai remover apenas os registros específicos que atendem a uma 
+// determinada condição usando O WHERE .
+//O Where aqui ele vai deixar deletar somente os registros indicados pelo ('id').
 app.delete('/pessoas/:id', (req, res) => {
     const id = req.params.id;
     const sql = 'DELETE FROM pessoas WHERE id = ?';
@@ -124,7 +163,12 @@ app.delete('/pessoas/:id', (req, res) => {
     });
 });
 
-//lista as pessoas cadastradas e organiza pelo nome
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela.
+//O FROM, que indica a(s) tabela(s) dos quais recuperar dados.
+//ORDER BY organiza os resultados de acordo com uma ou mais colunas da tabela,
+//podendo definir a ordem do resultados como crescente ou decrescente.
+//Aqui o get lista as pessoas pela condição que foi informada no select cadastradas e organiza pelo nome
 app.get('/Lista_pessoas', (req, res) => {
     const sql = 'SELECT pessoas.nome, pessoas.data_nascimento, pessoas.telefone  FROM pessoas order by nome ';
     connection.query(sql, (error, results) => {
@@ -133,17 +177,37 @@ app.get('/Lista_pessoas', (req, res) => {
     });
 });
 
-//busca as tarefas pelo status e exibe en ordem pelo id das pessoas que foram cadastradas
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela.
+//O FROM, que indica a(s) tabela(s) dos quais recuperar dados.
+//ORDER BY organiza os resultados de acordo com uma ou mais colunas da tabela,
+//podendo definir a ordem do resultados como crescente ou decrescente.
+//'AS' é usado para atribuir um novo nome temporariamente a uma coluna de tabela.
+//Ele facilita a apresentação dos resultados da consulta e permite que o desenvolvedor rotule os resultados com mais precisão, 
+//sem renomear permanentemente as colunas da tabela.
+//O INNER JOIN seleciona registros que possuem valores correspondentes em ambas as tabelas.
+//busca as tarefas pelo status exibindo e organizando pelo id das pessoas que foram cadastradas,
+//mostrando o nome da pessoa que esta na tabela pessoas e tambem as tarefas relacionada a pessoa que esta na tabela tarefas.
 app.get('/Lista_tarefas/:status', (req, res) => {
     const status = req.params.status;
-    const sql = `SELECT pessoas.nome AS Responsável, tarefas.titulo, tarefas.descricao, tarefas.data_criacao, tarefas.data_conclusao, tarefas.status  FROM tarefas INNER JOIN pessoas  ON tarefas.id_pessoas = pessoas.id where status = ? ORDER BY Responsável `;
+    const sql = `SELECT pessoas.nome AS Responsável, tarefas.titulo, tarefas.descricao, tarefas.data_criacao, tarefas.data_conclusao, tarefas.status  FROM tarefas INNER JOIN pessoas  ON tarefas.id_pessoas = pessoas.id  where status = ? ORDER BY Responsável `;
     connection.query(sql, status, (error, results) => {
         if (error) throw error;
         res.json(results);
     });
 });
 
-// get para exibir todas as tarefas de uma pessoa especifica
+//método get ele retorna todos os registros do banco de dados.
+//SELECT para buscar os registros inseridos na tabela.
+//O FROM, que indica a(s) tabela(s) dos quais recuperar dados.
+//ORDER BY organiza os resultados de acordo com uma ou mais colunas da tabela,
+//podendo definir a ordem do resultados como crescente ou decrescente.
+//'AS' é usado para atribuir um novo nome temporariamente a uma coluna de tabela.
+//Ele facilita a apresentação dos resultados da consulta e permite que o desenvolvedor rotule os resultados com mais precisão, 
+//sem renomear permanentemente as colunas da tabela.
+//O INNER JOIN seleciona registros que possuem valores correspondentes em ambas as tabelas.
+// Busca os registros que estão na tabela pessoas e na tabela tarefas e exibi os resultados juntando as duas tabelas
+//que estão referenciadas pelo id da pessoa e o id da chave estrangeira fazendo a comparação entre os dois id.
 app.get('/pessoas_tarefas/:id', (req, res) => {
     const id = req.params.id;
     const sql = `SELECT p.id AS Código, p.nome, p.telefone, t.id AS código_Tarefa, t.titulo, t.descricao, t.status, t.data_criacao, t.data_conclusao FROM pessoas AS p INNER JOIN tarefas AS t ON t.id_pessoas = p.id  where p.id = ? `;
